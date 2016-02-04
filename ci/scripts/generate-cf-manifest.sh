fc94a27fb57350bb5eb68e6a57d9ee47e3401d43
Tiago Scolari <tscolari@pivotal.io>
2016-02-04 13:20:26 +0100

    Use flintstonecf/ci docker image in pipeline tasks
    [#113002695]
    
    Signed-off-by: Marc Schunk <Marc.Schunk@de.ibm.com>

--- ci/scripts/generate-cf-manifest.sh
+++ ci/scripts/generate-cf-manifest.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -ex
 
-apt-get install wget
-wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.7/spiff_linux_amd64.zip
-unzip spiff_linux_amd64.zip
-mv spiff /usr/local/bin/
-
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh status
