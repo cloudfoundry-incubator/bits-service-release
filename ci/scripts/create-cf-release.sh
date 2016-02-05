4bf6c147ee67b750a38a4e690abfc72872373773
Marc Schunk <Marc.Schunk@de.ibm.com>
2016-02-05 12:55:48 +0100

    create a dev release on the wip-bits-service branch
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/create-cf-release.sh
+++ ci/scripts/create-cf-release.sh
@@ -5,6 +5,10 @@ bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh status
 
 cd git-cf-release
+./scripts/update
 ./scripts/generate-bosh-lite-dev-manifest
+
+bosh create release --force --with-tarball
+mv dev_releases/cf/cf-*.tgz ../assets/release.tgz
 
 mv bosh-lite/deployments/cf.yml ../assets/manifest.yml
