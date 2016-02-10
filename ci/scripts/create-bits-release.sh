9515e2eeb9f7e6b66a48426787773eafa8be55ba
Marc Schunk <Marc.Schunk@de.ibm.com>
2016-02-10 17:45:20 +0000

    Use semver resource to build bits-service-release on CI
    [#112610587]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/create-bits-release.sh
+++ ci/scripts/create-bits-release.sh
@@ -6,17 +6,12 @@ bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh status
 
-bosh -n delete deployment bits-service
-set +e
-bosh -n delete release bits-service
-set -e
-
 ./scripts/generate-bosh-lite-manifest
 rm -rf ../assets/*
 
 mv deployments/bits-service-release-bosh-lite.yml ../assets/manifest.yml
 cat ../assets/manifest.yml
 
-rm -rf dev_releases
-bosh create release --force --name bits-service --with-tarball
+version=$(cat $VERSION_FILE)
+bosh create release --force --name bits-service --with-tarball --version $version
 mv dev_releases/bits-service/bits-service-*.tgz ../assets/
