44b57dcb9662f4f6a94669304b88492ec69ef6ce
Tiago Scolari <tscolari@pivotal.io>
2016-02-11 16:47:09 +0000

    Make CI less verbose
    Remove unecessary steps/verbosity from ci scripts

--- ci/scripts/create-bits-release.sh
+++ ci/scripts/create-bits-release.sh
@@ -4,13 +4,9 @@ cd $(dirname $0)/../../
 
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
-bosh status
 
 ./scripts/generate-bosh-lite-manifest
-rm -rf ../assets/*
-
 mv deployments/bits-service-release-bosh-lite.yml ../assets/manifest.yml
-cat ../assets/manifest.yml
 
 version=$(cat $VERSION_FILE)
 bosh create release --force --name bits-service --with-tarball --version $version
