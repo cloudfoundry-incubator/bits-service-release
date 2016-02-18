4d289c23b578d7f83a18ed045778b767eed3d7ee
Tiago Scolari <tscolari@pivotal.io>
2016-02-18 16:39:01 +0000

    Refactor the backend templates
    [#113813187]
    
    Signed-off-by: Marc Schunk <Marc.Schunk@de.ibm.com>

--- ci/scripts/create-bits-manifest.sh
+++ ci/scripts/create-bits-manifest.sh
@@ -9,7 +9,7 @@ if [ -z "$BLOBSTORE_TYPE" ]
 then
   ./scripts/generate-bosh-lite-manifest
 else
-  ./scripts/generate-bosh-lite-manifest ./templates/$BLOBSTORE_TYPE-blobstore-test.yml
+  ./scripts/generate-bosh-lite-manifest ./templates/$BLOBSTORE_TYPE.yml
 fi
 
 ./scripts/manifest_parser.rb deployments/bits-service-release-bosh-lite.yml > "../manifests/manifest.yml"
