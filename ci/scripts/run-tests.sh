7379910ad2ef578c2e028f58c3c1a7e64a378d9a
Tiago Scolari <tscolari@pivotal.io>
2016-02-18 15:31:21 +0000

    Make nginx work with custom `directory_key`
    [#113813187]
    
    Signed-off-by: Marc Schunk <Marc.Schunk@de.ibm.com>

--- ci/scripts/run-tests.sh
+++ ci/scripts/run-tests.sh
@@ -2,5 +2,11 @@
 
 cd $(dirname $0)/../../
 
+export BITS_SERVICE_MANIFEST=./manifest.yml
+
+bosh -u x -p x target $BOSH_TARGET Lite
+bosh login $BOSH_USERNAME $BOSH_PASSWORD
+bosh download manifest $RELEASE_NAME $BITS_SERVICE_MANIFEST
+
 bundle install
 bundle exec rspec spec
