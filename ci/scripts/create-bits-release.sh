4c7a43cca28804987cba352d078d840ea523d347
Tiago Scolari <tscolari@pivotal.io>
2016-02-18 13:41:27 +0000

    Rollback changes in the create release script
    Add `manifests` and `releases` inputs to deploy and upload tasks
    
    [#113813187]
    
    Signed-off-by: Marc Schunk <Marc.Schunk@de.ibm.com>

--- ci/scripts/create-bits-release.sh
+++ ci/scripts/create-bits-release.sh
@@ -3,5 +3,5 @@
 cd $(dirname $0)/../../
 
 version=$(cat $VERSION_FILE)
-bosh create release --force --name $RELEASE_NAME --with-tarball --version $version
-mv dev_releases/$RELEASE_NAME/$RELEASE_NAME-*.tgz ../releases/
+bosh create release --force --name bits-service --with-tarball --version $version
+mv dev_releases/bits-service/bits-service-*.tgz ../releases/
