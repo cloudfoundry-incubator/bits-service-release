5e59c94a7197efaeac148bde67194d851294a0ae
Tiago Scolari <tscolari@pivotal.io>
2016-02-18 11:52:09 +0000

    Fix the output folder for create-release on CI

--- ci/scripts/create-bits-release.sh
+++ ci/scripts/create-bits-release.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)/../../
 
 version=$(cat $VERSION_FILE)
 bosh create release --force --name $RELEASE_NAME --with-tarball --version $version
-mv dev_releases/$RELEASE_NAME/$RELEASE_NAME-*.tgz ../release/
+mv dev_releases/$RELEASE_NAME/$RELEASE_NAME-*.tgz ../releases/
