d36e993d14218866a7a12cd8ca4d7a8d691265b8
Stev Witzel <switzel@pivotal.io>
2016-02-23 14:04:36 +0000

    Use semver resource for release version in CI
    * set release version env variable in CI script deploy-release.sh
    * template bit-service release version in bosh manifest from env var
    * use task file deploy-release.yml in CI pipeline
    
    [#113825627]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/deploy-release.sh
+++ ci/scripts/deploy-release.sh
@@ -2,6 +2,10 @@
 
 cd $(dirname $0)/../../
 
+if [ -n "$RELEASE_VERSION_FILE" ]; then
+  export RELEASE_VERSION=`cat $RELEASE_VERSION_FILE`
+fi
+
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 
