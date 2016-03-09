44c1121b98a79818fe6283d97a2e77d784ff072f
Tiago Scolari <tscolari@pivotal.io>
2016-03-09 16:43:49 +0100

    Public dev release to SL ObjectStorage
    [#115144801]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

---
+++ ci/scripts/upload-to-object-storage.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+`gem install mime-types`
+
+require 'fog'
+
+s = Fog::Storage.new(
+  :provider => 'OpenStack',
+  :openstack_auth_url => ENV['OPENSTACK_AUTH_URL'],
+  :openstack_username => ENV['OPENSTACK_USER_NAME'],
+  :openstack_api_key  => ENV['OPENSTACK_API_KEY'],
+)
+
+release_files = Pathname.glob(ENV['RELEASE_FILE'])
+
+if 1 != release_files.size
+  warn "Expect only one RELEASE_FILE, but got #{release_files.size}: #{release_files}"
+  exit 1
+end
+
+file = release_files.first
+file_name = file.basename
+
+dir = s.directories.create key: 'releases', public: true
+remote_file = dir.files.create key: file_name.to_s, body: file.open, public: true
+
+puts "Successfully uploaded #{file_name} as #{remote_file.public_url}"
