16e1fadc45e98197a41960864547ce40d191bcb3
Tiago Scolari <tscolari@pivotal.io>
2016-03-10 12:16:32 +0100

    Generalize folder name and glob for input file
    [#115144847]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- ci/scripts/upload-to-object-storage.rb
+++ ci/scripts/upload-to-object-storage.rb
@@ -4,22 +4,22 @@ require 'fog'
 
 s = Fog::Storage.new(
   :provider => 'OpenStack',
-  :openstack_auth_url => ENV['OPENSTACK_AUTH_URL'],
-  :openstack_username => ENV['OPENSTACK_USER_NAME'],
-  :openstack_api_key  => ENV['OPENSTACK_API_KEY'],
+  :openstack_auth_url => ENV.fetch('OPENSTACK_AUTH_URL'),
+  :openstack_username => ENV.fetch('OPENSTACK_USER_NAME'),
+  :openstack_api_key  => ENV.fetch('OPENSTACK_API_KEY'),
 )
 
-release_files = Pathname.glob(ENV['RELEASE_FILE'])
+files = Pathname.glob(ENV.fetch('FILE_GLOB'))
 
-if 1 != release_files.size
-  warn "Expect only one RELEASE_FILE, but got #{release_files.size}: #{release_files}"
+if 1 != files.size
+  warn "Expect only one FILE_GLOB, but got #{files.size}: #{files}"
   exit 1
 end
 
-file = release_files.first
+file = files.first
 file_name = file.basename
 
-dir = s.directories.create key: 'releases', public: true
+dir = s.directories.create key: ENV.fetch('REMOTE_FOLDER'), public: true
 remote_file = dir.files.create key: file_name.to_s, body: file.open, public: true
 
 puts "Successfully uploaded #{file_name} as #{remote_file.public_url}"
