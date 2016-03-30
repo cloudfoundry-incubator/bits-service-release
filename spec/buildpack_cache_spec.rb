92fd986f011513f8ae692105d57645375361d58d
Tiago Scolari <tscolari@pivotal.io>
2016-03-30 15:24:23 +0100

    Add DELETE /buildpack_cache endpoint configuration
    [finishes #115151739]

--- spec/buildpack_cache_spec.rb
+++ spec/buildpack_cache_spec.rb
@@ -12,7 +12,7 @@ describe 'buildpack cache resource', type: :integration do
   let(:app_name) { "#{SecureRandom.uuid}/linux" }
   let(:endpoint) { '/buildpack_cache/' + app_name }
 
-  describe 'PUT /buildpack_cache' do
+  describe 'PUT /buildpack_cache/:app_guid/:stack_name' do
     it 'returns HTTP status 201' do
       response = make_put_request(endpoint, upload_body)
       expect(response.code).to eq(201)
@@ -31,7 +31,7 @@ describe 'buildpack cache resource', type: :integration do
     end
   end
 
-  describe 'DELETE /buildpack_cache' do
+  describe 'DELETE /buildpack_cache/:app_guid/:stack_name' do
     context 'when deleting an known file' do
       before do
         make_put_request(endpoint, upload_body)
@@ -63,7 +63,30 @@ describe 'buildpack cache resource', type: :integration do
     end
   end
 
-  describe 'GET /buildpack_cache' do
+  describe 'DELETE /buildpack_cache' do
+
+    let(:key1) { "#{SecureRandom.uuid}/some-stack-name" }
+    let(:key2) { "#{SecureRandom.uuid}/some-stack-name" }
+
+    before do
+      [key1, key2].each do |key|
+        make_put_request("/buildpack_cache/#{key}", { buildpack_cache: File.new(zip_filepath) })
+      end
+    end
+
+    it 'returns HTTP status 204' do
+      response = make_delete_request('/buildpack_cache')
+      expect(response.code).to eq(204)
+    end
+
+    it 'removes all the stored files' do
+      [key1, key2].each { |key| expect(blobstore_client.key_exist? key).to eq(true) }
+      make_delete_request('/buildpack_cache')
+      [key1, key2].each { |key| expect(blobstore_client.key_exist? key).to eq(false) }
+    end
+  end
+
+  describe 'GET /buildpack_cache/:app_guid/:stack_name' do
     context 'when getting a known file' do
       before do
         make_put_request(endpoint, upload_body)
