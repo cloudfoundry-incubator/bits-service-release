88006414ee38a4cc9996fb2a1754cbc93ab39a33
Tiago Scolari <tscolari@pivotal.io>
2016-04-01 16:26:36 +0100

    Add create-cf-release task
    This is an extraction from the inline task we had in the pipeline.yml

--- ci/scripts/create-cf-release.sh
+++ ci/scripts/create-cf-release.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)/../../../git-cf-release
 version=$(cat $VERSION_FILE)
 
 bosh create release --force --name cf --with-tarball --version $version
-mv dev_releases/cf/cf-*.tgz ../assets/release.tgz
+mv dev_releases/cf/cf-*.tgz ../releases/release.tgz
