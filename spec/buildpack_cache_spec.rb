5d7f7df661cfaf922104579924b1622349f11781
Tiago Scolari <tscolari@pivotal.io>
2016-04-11 13:48:34 +0100

    Rename buildpack_cache to buildpack_cache_entries
    [#116609895]
    
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- spec/buildpack_cache_spec.rb
+++ spec/buildpack_cache_spec.rb
@@ -9,9 +9,9 @@ describe 'buildpack cache resource', type: :integration do
   let(:blobstore_client) { backend_client(:buildpack_cache) }
 
   let(:app_name) { "#{SecureRandom.uuid}/linux" }
-  let(:endpoint) { '/buildpack_cache/' + app_name }
+  let(:endpoint) { '/buildpack_cache/entries/' + app_name }
 
-  describe 'PUT /buildpack_cache/:app_guid/:stack_name' do
+  describe 'PUT /buildpack_cache/entries/:app_guid/:stack_name' do
     it 'returns HTTP status 201' do
       response = make_put_request(endpoint, upload_body)
       expect(response.code).to eq(201)
@@ -30,7 +30,7 @@ describe 'buildpack cache resource', type: :integration do
     end
   end
 
-  describe 'DELETE /buildpack_cache/:app_guid/:stack_name' do
+  describe 'DELETE /buildpack_cache/entries/:app_guid/:stack_name' do
     context 'when deleting an known file' do
       before do
         make_put_request(endpoint, upload_body)
@@ -48,7 +48,7 @@ describe 'buildpack cache resource', type: :integration do
     end
 
     context 'when deleting an unknown file' do
-      let(:endpoint) { '/buildpack_cache/unknown_app/linux' }
+      let(:endpoint) { '/buildpack_cache/entries/unknown_app/linux' }
 
       it 'returns HTTP status 404' do
         response = make_delete_request(endpoint)
@@ -62,29 +62,29 @@ describe 'buildpack cache resource', type: :integration do
     end
   end
 
-  describe 'DELETE /buildpack_cache' do
+  describe 'DELETE /buildpack_cache/entries' do
     let(:key1) { "#{SecureRandom.uuid}/some-stack-name" }
     let(:key2) { "#{SecureRandom.uuid}/some-stack-name" }
 
     before do
       [key1, key2].each do |key|
-        make_put_request("/buildpack_cache/#{key}", { buildpack_cache: File.new(zip_filepath) })
+        make_put_request("/buildpack_cache/entries/#{key}", { buildpack_cache: File.new(zip_filepath) })
       end
     end
 
     it 'returns HTTP status 204' do
-      response = make_delete_request('/buildpack_cache')
+      response = make_delete_request('/buildpack_cache/entries')
       expect(response.code).to eq(204)
     end
 
     it 'removes all the stored files' do
       [key1, key2].each { |key| expect(blobstore_client.key_exist?(key)).to eq(true) }
-      make_delete_request('/buildpack_cache')
+      make_delete_request('/buildpack_cache/entries')
       [key1, key2].each { |key| expect(blobstore_client.key_exist?(key)).to eq(false) }
     end
   end
 
-  describe 'GET /buildpack_cache/:app_guid/:stack_name' do
+  describe 'GET /buildpack_cache/entries/:app_guid/:stack_name' do
     context 'when getting a known file' do
       before do
         make_put_request(endpoint, upload_body)
@@ -102,7 +102,7 @@ describe 'buildpack cache resource', type: :integration do
     end
 
     context 'when getting an unknown file' do
-      let(:endpoint) { '/buildpack_cache/unknown_app/linux' }
+      let(:endpoint) { '/buildpack_cache/entries/unknown_app/linux' }
 
       it 'returns HTTP status 404' do
         response = make_get_request(endpoint)
