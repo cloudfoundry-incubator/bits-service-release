abf4af2996e31363c3d2199114aad85b20652a12
Marc Schunk <Marc.Schunk@de.ibm.com>
2016-04-11 17:32:35 +0200

    be more verbose in manifest creation
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- ci/scripts/update-cf-manifest.sh
+++ ci/scripts/update-cf-manifest.sh
@@ -2,4 +2,8 @@
 
 cd $(dirname $0)/../../
 
+set -x
+echo MANIFEST_STUBS: ${MANIFEST_STUBS}
+find .
+
 spruce merge ./ci/manifests/cf.yml ./ci/manifests/tweaks.yml ${MANIFEST_STUBS} > ../manifests/manifest.yml
