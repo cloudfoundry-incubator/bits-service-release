dca797d6867f47ee619f1f343dae0664a49ac0a3
Marc Schunk <Marc.Schunk@de.ibm.com>
2016-04-12 16:32:10 +0100

    Sync cf-release bosh blobs in parallel
    [#113466423]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/create-cf-release.sh
+++ ci/scripts/create-cf-release.sh
@@ -4,5 +4,6 @@ cd $(dirname $0)/../../../git-cf-release
 
 version=$(cat $VERSION_FILE)
 
+bosh -n --parallel 10 sync blobs
 bosh create release --force --name cf --with-tarball --version $version
 mv dev_releases/cf/cf-*.tgz ../releases/release.tgz
