c530c90c30ee3098a518ca682fb8262a3902e541
Stev Witzel <switzel@pivotal.io>
2016-04-12 16:00:25 +0200

    Remove diagnostic code
    [#116143345]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- ci/scripts/update-cf-manifest.sh
+++ ci/scripts/update-cf-manifest.sh
@@ -2,8 +2,4 @@
 
 cd $(dirname $0)/../../
 
-set -x
-echo MANIFEST_STUBS: ${MANIFEST_STUBS}
-find .
-
 spruce merge ./ci/manifests/cf.yml ./ci/manifests/tweaks.yml ${MANIFEST_STUBS} > ../manifests/manifest.yml
