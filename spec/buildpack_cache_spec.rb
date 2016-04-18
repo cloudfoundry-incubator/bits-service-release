cef4669140f70e8101cf195728ce6c11d9799b72
Marc Schunk <Marc.Schunk@de.ibm.com>
2016-04-18 13:16:20 +0200

    Add tests for `DELETE /buildpack_cache/entries/:app_guid`
    [#116898079]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- spec/buildpack_cache_spec.rb
+++ spec/buildpack_cache_spec.rb
@@ -8,56 +8,85 @@ describe 'buildpack cache resource', type: :integration do
   let(:upload_body) { { buildpack_cache: zip_file } }
   let(:blobstore_client) { backend_client(:buildpack_cache) }
 
-  let(:app_name) { "#{SecureRandom.uuid}/linux" }
-  let(:endpoint) { '/buildpack_cache/entries/' + app_name }
+  let(:app_guid) { SecureRandom.uuid }
+  let(:app_name) { "#{app_guid}/linux" }
+  let(:path) { '/buildpack_cache/entries/' + app_name }
 
   describe 'PUT /buildpack_cache/entries/:app_guid/:stack_name' do
     it 'returns HTTP status 201' do
-      response = make_put_request(endpoint, upload_body)
+      response = make_put_request(path, upload_body)
       expect(response.code).to eq(201)
     end
 
     it 'stores the blob in the backend' do
-      make_put_request(endpoint, upload_body)
+      make_put_request(path, upload_body)
       expect(blobstore_client.key_exist?(app_name)).to eq(true)
     end
 
     context 'when the request body is invalid' do
       it 'returns HTTP status 415' do
-        response = make_put_request(endpoint, {})
+        response = make_put_request(path, {})
         expect(response.code).to eq(415)
       end
     end
   end
 
-  describe 'DELETE /buildpack_cache/entries/:app_guid/:stack_name' do
+  describe 'DELETE specific entry for /buildpack_cache/entries/:app_guid/:stack_name' do
     context 'when deleting an known file' do
       before do
-        make_put_request(endpoint, upload_body)
+        make_put_request(path, upload_body)
       end
 
       it 'returns HTTP status 204' do
-        response = make_delete_request(endpoint)
+        response = make_delete_request(path)
         expect(response.code).to eq(204)
       end
 
       it 'deletes the blob from the backend' do
-        make_delete_request(endpoint)
+        make_delete_request(path)
         expect(blobstore_client.key_exist?(app_name)).to eq(false)
       end
     end
 
     context 'when deleting an unknown file' do
-      let(:endpoint) { '/buildpack_cache/entries/unknown_app/linux' }
+      let(:path) { '/buildpack_cache/entries/unknown_app/linux' }
 
       it 'returns HTTP status 404' do
-        response = make_delete_request(endpoint)
+        response = make_delete_request(path)
         expect(response).to be_a_404
       end
 
       it 'returns the correct error' do
-        response = make_delete_request(endpoint)
+        response = make_delete_request(path)
         expect(response).to be_a_404
+      end
+    end
+  end
+
+  describe 'DELETE all entries for /buildpack_cache/entries/:app_guid' do
+    let(:delete_path) { '/buildpack_cache/entries/' + app_guid }
+    context 'when deleting an known file' do
+      before do
+        make_put_request(path, upload_body)
+      end
+
+      it 'returns HTTP status 204' do
+        response = make_delete_request(delete_path)
+        expect(response.code).to eq(204)
+      end
+
+      it 'deletes the blob from the backend' do
+        make_delete_request(delete_path)
+        expect(blobstore_client.key_exist?(app_name)).to eq(false)
+      end
+    end
+
+    context 'when deleting an unknown file' do
+      let(:path) { '/buildpack_cache/entries/unknown_app' }
+
+      it 'returns HTTP status 404' do
+        response = make_delete_request(path)
+        expect(response.code).to eq(204)
       end
     end
   end
@@ -87,30 +116,30 @@ describe 'buildpack cache resource', type: :integration do
   describe 'GET /buildpack_cache/entries/:app_guid/:stack_name' do
     context 'when getting a known file' do
       before do
-        make_put_request(endpoint, upload_body)
+        make_put_request(path, upload_body)
       end
 
       it 'returns HTTP status 200' do
-        response = make_get_request(endpoint)
+        response = make_get_request(path)
         expect(response.code).to eq(200)
       end
 
       it 'returns the correct contents' do
-        response = make_get_request(endpoint)
+        response = make_get_request(path)
         expect(response.body).to eq File.open(zip_filepath, 'rb').read
       end
     end
 
     context 'when getting an unknown file' do
-      let(:endpoint) { '/buildpack_cache/entries/unknown_app/linux' }
+      let(:path) { '/buildpack_cache/entries/unknown_app/linux' }
 
       it 'returns HTTP status 404' do
-        response = make_get_request(endpoint)
+        response = make_get_request(path)
         expect(response.code).to eq(404)
       end
 
       it 'returns the correct error' do
-        response = make_get_request(endpoint)
+        response = make_get_request(path)
         expect(response).to be_a_404
       end
     end
