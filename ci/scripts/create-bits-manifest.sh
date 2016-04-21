c63632ea01e2a872c3a65bc87a0ab052df41a65d
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-04-21 11:42:29 +0100

    Add version to bits-service manifest generation
    [#117927603]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/create-bits-manifest.sh
+++ ci/scripts/create-bits-manifest.sh
@@ -2,6 +2,14 @@
 
 cd $(dirname $0)/../../
 
+if [ -e "$VERSION_FILE" ]; then
+  export VERSION=$(cat $VERSION_FILE)
+  echo "Using VERSION=\"$VERSION\""
+else
+  echo "The \$VERSION_FILE \"$VERSION_FILE\" does not exist"
+  exit 1
+fi
+
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 
@@ -12,4 +20,4 @@ else
   ./scripts/generate-test-bosh-lite-manifest ./templates/$BLOBSTORE_TYPE.yml
 fi
 
-cp deployments/bits-service-release.yml ../manifests/manifest.yml
+cp deployments/bits-service-release.yml "../manifests/manifest-$VERSION_FILE.yml"
