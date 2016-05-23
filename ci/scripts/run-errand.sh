e9472e684dca8a793ab7b599a73917eb06eea068
Peter Goetz <pego@de.ibm.com>
2016-05-23 16:53:03 +0100

    Run errand accept deployment name as arg
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/run-errand.sh
+++ ci/scripts/run-errand.sh
@@ -3,7 +3,7 @@
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 
-bosh download manifest cf-warden > manifest.yml
+bosh download manifest ${DEPLOYMENT_NAME:-cf-warden} > manifest.yml
 bosh deployment manifest.yml
 
 bosh run errand $ERRAND_NAME --keep-alive
