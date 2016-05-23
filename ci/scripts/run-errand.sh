8d69d4ae20a62fbd30585b1d5fc85203dfcb46da
Peter Goetz <pego@de.ibm.com>
2016-05-23 11:37:40 +0200

    Use keep-alive when running CAT errand
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/run-errand.sh
+++ ci/scripts/run-errand.sh
@@ -6,4 +6,4 @@ bosh login $BOSH_USERNAME $BOSH_PASSWORD
 bosh download manifest cf-warden > manifest.yml
 bosh deployment manifest.yml
 
-bosh run errand $ERRAND_NAME
+bosh run errand $ERRAND_NAME --keep-alive
