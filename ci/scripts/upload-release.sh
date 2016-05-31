5255b59a1f4ad22be1881e9ae2ff5fd6b619a39c
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-05-31 14:41:18 +0100

    Skip release upload if it already exists
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/upload-release.sh
+++ ci/scripts/upload-release.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)/../../
 bosh -u x -p x target $BOSH_TARGET Lite
 bosh login $BOSH_USERNAME $BOSH_PASSWORD
 
-bosh upload release $RELEASE_FILE
+bosh upload release $RELEASE_FILE --skip-if-exists
