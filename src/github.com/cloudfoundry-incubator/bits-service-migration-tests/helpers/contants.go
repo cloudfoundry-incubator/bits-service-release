ae6d06fcab1ca49269039bdb9335764be67ef5c9
Tiago Scolari <tscolari@pivotal.io>
2016-06-20 15:46:44 +0100

    Add buildpack_cache migration tests for BSMT
    [#120659409]
    
    Signed-off-by: Peter Gotz <PEGO@de.ibm.com>

--- src/github.com/cloudfoundry-incubator/bits-service-migration-tests/helpers/contants.go
+++ src/github.com/cloudfoundry-incubator/bits-service-migration-tests/helpers/contants.go
@@ -1,9 +1,11 @@
 package helpers
 
 const (
-	BuildpackName      = "bsmt-test-buildpack"
-	BuildpackPath      = "../assets/buildpack.zip"
-	TestAppPath        = "../assets/staticfile"
-	DropletTestAppName = "BSMT-droplet-test-app"
-	PackageTestAppName = "BSMT-package-test-app"
+	BuildpackName               = "bsmt-test-buildpack"
+	BuildpackPath               = "../assets/buildpack.zip"
+	TestAppPath                 = "../assets/staticfile"
+	DropletTestAppName          = "BSMT-droplet-test-app"
+	PackageTestAppName          = "BSMT-package-test-app"
+	BuildpackCacheBuildpackName = "BSMT-buildpack-cache-buildpack"
+	BuildpackCacheAppName       = "BSMT-buildpack-cache-app"
 )
