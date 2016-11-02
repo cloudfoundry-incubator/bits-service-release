75e3a42e4191594da020764fe5c8d78c88a3d81a
Peter Goetz <peter.gtz@gmail.com>
2016-11-02 13:35:18 +0100

    Refactor signing_spec.rb to simplify adding more test cases

--- spec/signing_spec.rb
+++ spec/signing_spec.rb
@@ -1,82 +1,47 @@
 require 'spec_helper'
 
 context 'URL Signing', type: :integration do
-  let(:zip_filepath) { File.expand_path('../assets/empty.zip', __FILE__) }
-  let(:zip_file) { File.new(zip_filepath) }
-  let(:upload_body) { { package: zip_file } }
-  let(:guid) { SecureRandom.uuid }
-  let(:path) { "/packages/#{guid}" }
-  let(:sign_url) { "#{private_endpoint}/sign#{path}" }
+  let(:path) { "/packages/#{SecureRandom.uuid}" }
 
   before do
-    response = make_put_request(path, upload_body)
-    expect(response.code).to eq(201)
+    # TODO: (pego) Why does this work without authentication?
+    RestClient.put("#{private_endpoint}#{path}", { package: File.new(File.expand_path('../assets/empty.zip', __FILE__))  })
   end
 
   describe '/sign' do
-    let(:sign_response) do
-      make_get_request('', {
-        method: :get,
-        url: sign_url,
-        user: signing_username,
-        password: signing_password,
-        verify_ssl: false
-      })
-    end
-
-    it 'returns HTTP success' do
-      expect(sign_response.code).to eq 200
-    end
-
     it 'returns a signed URL' do
-      signed_url = sign_response.body.to_s
+      response = RestClient.get("http://#{signing_username}:#{signing_password}@#{private_endpoint.hostname}/sign#{path}")
 
+      expect(response.code).to eq 200
+
+      signed_url = response.body.to_s
       expect(signed_url).to match(/.*md5=.*/).or match(/X-Amz-Signature=/)
       expect(signed_url).to match(/.*expires=.*/).or match(/X-Amz-Expires=/)
     end
 
     context 'when the signing credentials are incorrect' do
-      let(:signing_password) { 'wrong_password' }
-
       it 'returns HTTP status code 401' do
-        expect(sign_response.code).to eq 401
+        expect{
+          RestClient.get("http://#{signing_username}:WRONG_PASSWORD@#{private_endpoint.hostname}/sign#{path}")
+        }.to raise_error RestClient::Unauthorized
       end
     end
   end
 
   describe '/signed' do
     context 'when the signature is valid' do
-      let(:signed_url) do
-        make_get_request('', {
-            method: :get,
-            url: sign_url,
-            user: signing_username,
-            password: signing_password,
-            verify_ssl: false
-        }).body.to_s
-      end
-
       it 'resolves the signed_url and handles the request' do
-        response = RestClient::Request.execute(
-          url: signed_url,
-          method: :get,
-          verify_ssl: false
-        )
+        signed_url = RestClient.get("http://#{signing_username}:#{signing_password}@#{private_endpoint.hostname}/sign#{path}").body.to_s
+        response = RestClient.get(signed_url)
         expect(response.code).to eq 200
       end
     end
 
     context 'when the signature is invaid' do
-      let(:signed_url) { "#{public_endpoint}/signed#{path}?md5=Eg1N60x4bZigd_E3BjXE1Q&expires=1467828099" }
-
       it 'returns a 403' do
-        response = make_get_request('', {
-          url: signed_url,
-          method: :get,
-          verify_ssl: false,
-          headers: {}
-        })
-        expect(response.code).to eq 403
+        expect {
+          RestClient.get("#{public_endpoint}/signed#{path}?md5=INVALID_SIGNATURE&expires=1467828099")
+        }.to raise_error RestClient::Forbidden
       end
     end
   end
