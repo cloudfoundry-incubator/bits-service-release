9e2bebc5f3a71440492072c11ee1bb8f80e6c81b
Peter Goetz <peter.gtz@gmail.com>
2018-01-10 00:16:40 +0100

    Re-add load_private_endpoint_ip
    That method accidentally got deleted while resolving conflicts during rebase.

--- spec/support/environment.rb
+++ spec/support/environment.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+def load_private_endpoint_ip
+  ENV.fetch('BITS_SERVICE_PRIVATE_ENDPOINT_IP')
+rescue KeyError
+  raise 'BITS_SERVICE_PRIVATE_ENDPOINT_IP not set; please set it the IP address of the bits-service job.'
+end
+
 module EnvironmentHelpers
   @@private_endpoint_ip = load_private_endpoint_ip
 
