5d9af15bb3d2a4d2ba8c347211872c15192c2657
Peter Goetz <peter.gtz@gmail.com>
2018-04-09 11:25:50 +0200

    Use pkill in statsd_pipe to kill complete process tree
    We saw some orphaned processes hanging around after monit restart and it seems that pkill --parent PID makes sure everything is properly killed.
    
    [#156572077]
    
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- src/bits-common/pid_utils.sh
+++ src/bits-common/pid_utils.sh
@@ -119,3 +119,45 @@ function kill_and_wait() {
     rm -f "${pidfile}"
   fi
 }
+
+function pkill_and_wait() {
+  declare pidfile="$1" timeout="${2:-25}" sigkill_on_timeout="${3:-1}"
+
+  if [ ! -f "${pidfile}" ]; then
+    echo "Pidfile ${pidfile} doesn't exist"
+    exit 0
+  fi
+
+  local pid
+  pid=$(head -1 "${pidfile}")
+
+  if [ -z "${pid}" ]; then
+    echo "Unable to get pid from ${pidfile}"
+    exit 1
+  fi
+
+  if ! pid_is_running "${pid}"; then
+    echo "Process ${pid} is not running"
+    rm -f "${pidfile}"
+    exit 0
+  fi
+
+  echo "pkilling ${pidfile}: ${pid} "
+  pkill -P "${pid}"
+
+  if ! wait_pid_death "${pid}" "${timeout}"; then
+    if [ "${sigkill_on_timeout}" = "1" ]; then
+      echo "pkill timed out, using pkill -9 on ${pid}"
+      pkill -9 -P "${pid}"
+      sleep 0.5
+    fi
+  fi
+
+  if pid_is_running "${pid}"; then
+    echo "Timed Out"
+    exit 1
+  else
+    echo "Stopped"
+    rm -f "${pidfile}"
+  fi
+}
