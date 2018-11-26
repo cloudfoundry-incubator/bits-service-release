23a768c363436b4bb78799b7671493912052ac51
Peter Goetz <peter.gtz@gmail.com>
2018-11-26 12:08:30 +0100

    Use trap for system test cleanup

--- scripts/run-system-tests.sh
+++ scripts/run-system-tests.sh
@@ -12,6 +12,14 @@ if [ "$1" == "webdav" -o "$1" == "local" ]; then
 : ${BOSH_DEPLOYMENT?"Please set bosh env variable BOSH_DEPLOYMENT to access the blobstore VM."}
 fi
 
+cleanup () {
+    pushd scripts/system-test-config
+        kill -9 $BITSGO_PID || true
+        rm -rf cert_file key_file ca_cert var-store.yml manifest.yml
+    popd
+}
+trap cleanup EXIT
+
 # build bitsgo
 pushd src/github.com/cloudfoundry-incubator/bits-service/cmd/bitsgo
     go install
@@ -33,7 +41,7 @@ pushd scripts/system-test-config
 
     BITS_LISTEN_ADDR=127.0.0.1 bitsgo -c <(spruce merge localhost-config.yml $1.yml) &
     BITSGO_PID=$!
-    sleep 1
+    sleep 2
 popd
 
 # generate config for tests
@@ -48,9 +56,3 @@ BITS_SERVICE_PRIVATE_ENDPOINT_IP=127.0.0.1 \
     BITS_SERVICE_CA_CERT=scripts/system-test-config/ca_cert \
     \
     bundle exec rspec $2
-
-# Cleanup
-pushd scripts/system-test-config
-    kill -9 $BITSGO_PID
-    rm -rf cert_file key_file ca_cert var-store.yml manifest.yml
-popd
