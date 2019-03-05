05104f4d49a74e93a692a6d4a4a864a8638644bf
Peter Goetz <peter.gtz@gmail.com>
2019-03-05 11:06:51 +0100

    Revert "Remove docker copy / update reference to docker image"
    This reverts commit 81e961e63552f7874c65be07974466e449e06592.

--- docker/generate-docker-image.sh
+++ docker/generate-docker-image.sh
@@ -2,4 +2,4 @@
 
 GOOS=linux GOARCH=amd64 go build -o bitsgo github.com/cloudfoundry-incubator/bits-service/cmd/bitsgo
 
-docker build . -t "flintstonecf/bits-service:2.24.0"
+docker build . -t "flintstonecf/bits-service"
