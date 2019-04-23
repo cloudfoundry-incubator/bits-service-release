131bedde50880bd7b33820b62775c4b6d299f43f
Kiran Jain <kiran.jain2@ibm.com>
2019-04-23 23:00:33 +0200

    Update Erini Download script to take version as input
    [#164168224]

--- docker/eirini-rootfs-downloader/eirini-rootfs-downloader.sh
+++ docker/eirini-rootfs-downloader/eirini-rootfs-downloader.sh
@@ -1,67 +1,89 @@
 #!/bin/bash -ex
 
-LATEST_ROOTFS="-1"
 ASSETS_PATH="/assets"
 
 function main
 {
-    create_etag_file_if_non_existent
-    ETAG_VALUE=$(cat ${ASSETS_PATH}/eirini_rootfs_etag_latest)
-    if [ ! -s ${ASSETS_PATH}/eirini_rootfs_etag_latest ]; then
-        echo "eirini_rootfs_etag_latest not set - will download latest rootfs from GIT"
-        grep_and_persist_etag
+    abort_if_eirini_rootfs_version_not_set
+
+    verify_rootfs_version_exists
+
+    create_version_file_if_non_existent
+
+    VERSION_VALUE=$(cat ${ASSETS_PATH}/eirini_rootfs_version_file)
+    if [ ! -s ${ASSETS_PATH}/eirini_rootfs_version_file ]; then
+        echo "eirini_rootfs_version not set - will download specified version ($EIRINI_ROOTFS_VERSION) of rootfs from GIT"
+        grep_and_persist_version
         download_rootfs_tar
     else
-        is_latest_rootfs
-        if [ "$LATEST_ROOTFS" == "1" ]; then
-            echo "RootFS is up to date - no action required"
-            return 0
-        elif [ "$LATEST_ROOTFS" == "0" ]; then
-            echo "RootFS is outdated - Downloading..."
-            if [ -f ${ASSETS_PATH}/eirinifs.tar ]; then
-                echo "Found outdated RootFS - deleting it"
-                rm -rf ${ASSETS_PATH}/eirinifs.tar
-            fi
-            grep_and_persist_etag
-            download_rootfs_tar
-            return 0
-        else
-            echo "There was an error calling the GITHUB API"
-            return 1
-        fi
+        is_specified_rootfs_version_present
+        echo "RootFS is outdated - Downloading..."
+        delete_eirini_rootfs_tar_if_present
+        grep_and_persist_version
+        download_rootfs_tar
+        return 0
     fi
 }
 
-function create_etag_file_if_non_existent
+function abort_if_eirini_rootfs_version_not_set
 {
-    if [ ! -f ${ASSETS_PATH}/eirini_rootfs_etag_latest ]; then
-        touch ${ASSETS_PATH}/eirini_rootfs_etag_latest
+    if [[ -z "${EIRINI_ROOTFS_VERSION}" ]]; then
+        return 1
+        #todo: think about providing a meaningful message like "not set - needs to be set - aborting"
     fi
 }
 
-function grep_and_persist_etag
+function create_version_file_if_non_existent
 {
-    curl https://api.github.com/repos/cloudfoundry-incubator/eirinifs/releases/latest -L -i | grep ETag: | cut -d \"  -f 2 > ${ASSETS_PATH}/eirini_rootfs_etag_latest
+    if [ ! -f ${ASSETS_PATH}/eirini_rootfs_version_file ]; then
+        touch ${ASSETS_PATH}/eirini_rootfs_version_file
+    fi
+}
+
+function grep_and_persist_version
+{
+    echo $EIRINI_ROOTFS_VERSION > ${ASSETS_PATH}/eirini_rootfs_version_file
 }
 
 function download_rootfs_tar
 {
-    curl -X GET -L $(curl https://api.github.com/repos/cloudfoundry-incubator/eirinifs/releases/latest | jq '.assets[].browser_download_url' | tr -d '\"') --output ${ASSETS_PATH}/eirinifs.tar
+    curl -X GET -L $(curl https://api.github.com/repos/cloudfoundry-incubator/eirinifs/releases/tags/${EIRINI_ROOTFS_VERSION} | jq '.assets[].browser_download_url' | tr -d '\"') --output ${ASSETS_PATH}/eirinifs.tar
 }
 
-function is_latest_rootfs
+function is_specified_rootfs_version_present
 {
-    LATEST=$(cat ${ASSETS_PATH}/eirini_rootfs_etag_latest)
-    HTTP_STATUS_CODE=$(curl -I -L https://api.github.com/repos/cloudfoundry-incubator/eirinifs/releases/latest --header "If-None-Match: ${LATEST}" | grep "Status")
-    if [ "$HTTP_STATUS_CODE"=="Status: 200 OK" ]
+    LOCAL_version=$(cat ${ASSETS_PATH}/eirini_rootfs_version_file)
+    REMOTE_version=${EIRINI_ROOTFS_VERSION}
+    if [ "$LOCAL_version" == "$REMOTE_version" ]
      then
-        LATEST_ROOTFS=1
-    elif [ "$HTTP_STATUS_CODE"=="Status: 304 OK" ]
-     then
-        LATEST_ROOTFS=0
+        echo "Already have the downloaded version ${EIRINI_ROOTFS_VERSION} "
+        exit 0
     else
-        LATEST_ROOTFS=ERROR
+        echo "Specified version is not there, need to download"
     fi
 }
 
+function verify_rootfs_version_exists 
+{
+    STATUS_CODE=$(curl -I -L https://api.github.com/repos/cloudfoundry-incubator/eirinifs/releases/tags/${EIRINI_ROOTFS_VERSION} --write-out  %{http_code} --silent --output /dev/null)
+    if [ "$STATUS_CODE" == "200" ]
+    then
+        echo "The specified rootfs version (${EIRINI_ROOTFS_VERSION}) exists --continue"
+    elif [ "$STATUS_CODE" == "404" ]
+     then
+        echo "The specified rootfs version (${EIRINI_ROOTFS_VERSION}) does not exist - please speficy an existing version"
+        exit 1
+    else
+        echo "Unexpected server response code : {STATUS_CODE} - aborting"
+        exit 1
+    fi
+}
+
+function delete_eirini_rootfs_tar_if_present
+{
+    if [ -f ${ASSETS_PATH}/eirinifs.tar ]; then
+        echo "Found outdated RootFS - deleting it"
+        rm -rf ${ASSETS_PATH}/eirinifs.tar
+    fi
+}
 main
