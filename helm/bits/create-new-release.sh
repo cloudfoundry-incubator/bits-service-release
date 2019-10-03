d69c49fc41e1f9233e2f439f36ef1d9a07d2eee6
Georgi Dankov <gddankov@gmail.com>
2019-10-03 14:36:03 +0100

    Use https
    [#166876339]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- helm/bits/create-new-release.sh
+++ helm/bits/create-new-release.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)/..
 
 helm package bits
-helm repo index . --url http://cloudfoundry-incubator.github.io/bits-service-release/helm
-helm repo add bits http://cloudfoundry-incubator.github.io/bits-service-release/helm
+helm repo index . --url https://cloudfoundry-incubator.github.io/bits-service-release/helm
+helm repo add bits https://cloudfoundry-incubator.github.io/bits-service-release/helm
 helm repo list
