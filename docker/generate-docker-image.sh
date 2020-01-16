d3f343727b3769ef5cb74d595c624b8689da2267
Oleksandr Slynko <oslynko@pivotal.io>
2020-01-16 17:02:50 +0200

    Run bits service with non root user
    [#170311869]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- docker/generate-docker-image.sh
+++ docker/generate-docker-image.sh
@@ -2,4 +2,4 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bitsgo github.com/cloudfoundry-incubator/bits-service/cmd/bitsgo
 
-docker build . -t "flintstonecf/bits-service"
+docker build . -t "eirini/bits-service" --build-arg GIT_SHA="$(git rev-parse HEAD)"
